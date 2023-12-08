a452497c3f6a21df242c2be6fd64da7a3f59cd0b
Andrzej Borkowski <kcv992@gmail.com>
2023-12-08 11:57:48 +0100

    Dzień 11

--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -1,4 +1,6 @@
-﻿namespace ChallengeApp
+﻿using System.Diagnostics;
+
+namespace ChallengeApp
 {
     public class Employee
     {
@@ -73,7 +75,24 @@
             var valueinfloat = (float)grade;
             AddGrade(valueinfloat);
         }
-        public Statistics GetStatistics()
+        public Statistics GetStatisticsWithFor()
+        {
+            var statisctics = new Statistics();
+            statisctics.Average = 0;
+            statisctics.Max = float.MinValue;
+            statisctics.Min = float.MaxValue;
+            
+            for (int i = 0; i < this.grades.Count; i++)
+            {
+                    statisctics.Max = Math.Max(statisctics.Max, grades[i]);
+                    statisctics.Min = Math.Min(statisctics.Min, grades[i]);
+                    statisctics.Average += grades[i];
+            }
+
+            statisctics.Average /= this.grades.Count;
+            return statisctics;
+        }
+        public Statistics GetStatisticsWithForEach()
         {
             var statisctics = new Statistics();
             statisctics.Average = 0;
@@ -87,6 +106,47 @@
                 statisctics.Average += grade;
             }
 
+            statisctics.Average /= this.grades.Count;
+            return statisctics;
+        }
+        public Statistics GetStatisticsWithWhile()
+        {
+            var statisctics = new Statistics();
+            statisctics.Average = 0;
+            statisctics.Max = float.MinValue;
+            statisctics.Min = float.MaxValue;
+
+            var i = 0;
+
+            while (i < this.grades.Count)
+            {
+                statisctics.Max = Math.Max(statisctics.Max, grades[i]);
+                statisctics.Min = Math.Min(statisctics.Min, grades[i]);
+                statisctics.Average += grades[i];
+                i++;
+            }
+
+            statisctics.Average /= this.grades.Count;
+            return statisctics;
+        }
+        public Statistics GetStatisticsWithDoWhile()
+        {
+            var statisctics = new Statistics();
+            statisctics.Average = 0;
+            statisctics.Max = float.MinValue;
+            statisctics.Min = float.MaxValue;
+
+            var i = 0;
+
+            do
+            {
+                statisctics.Max = Math.Max(statisctics.Max, grades[i]);
+                statisctics.Min = Math.Min(statisctics.Min, grades[i]);
+                statisctics.Average += grades[i];
+                i++;
+            }
+
+            while (i < this.grades.Count);
             statisctics.Average /= this.grades.Count;
             return statisctics;
         }
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -5,7 +5,27 @@ employee.AddGrade("Andrzej");
 employee.AddGrade("123123123");
 employee.AddGrade(45);
 employee.AddGrade(75);
-var statistics = employee.GetStatistics();
-Console.WriteLine($"Najlepsza ocena: {statistics.Max}");
-Console.WriteLine($"Najgorsza ocena: {statistics.Min}");
-Console.WriteLine($"Średnia z ocen: {statistics.Average:N2}");
+var statistics1 = employee.GetStatisticsWithFor();
+var statistics2 = employee.GetStatisticsWithForEach();
+var statistics3 = employee.GetStatisticsWithWhile();
+var statistics4 = employee.GetStatisticsWithDoWhile();
+
+Console.WriteLine("Pętla For");
+Console.WriteLine($"Najlepsza ocena: {statistics1.Max}");
+Console.WriteLine($"Najgorsza ocena: {statistics1.Min}");
+Console.WriteLine($"Średnia z ocen: {statistics1.Average:N2}");
+
+Console.WriteLine("Pętla For each");
+Console.WriteLine($"Najlepsza ocena: {statistics2.Max}");
+Console.WriteLine($"Najgorsza ocena: {statistics2.Min}");
+Console.WriteLine($"Średnia z ocen: {statistics2.Average:N2}");
+
+Console.WriteLine("Pętla While");
+Console.WriteLine($"Najlepsza ocena: {statistics3.Max}");
+Console.WriteLine($"Najgorsza ocena: {statistics3.Min}");
+Console.WriteLine($"Średnia z ocen: {statistics3.Average:N2}");
+
+Console.WriteLine("Pętla Do While");
+Console.WriteLine($"Najlepsza ocena: {statistics4.Max}");
+Console.WriteLine($"Najgorsza ocena: {statistics4.Min}");
+Console.WriteLine($"Średnia z ocen: {statistics4.Average:N2}");
