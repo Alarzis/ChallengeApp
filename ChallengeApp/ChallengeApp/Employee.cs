391fee4843e4941f8630073c418b34636d06aea0
Andrzej Borkowski <kcv992@gmail.com>
2024-01-08 18:28:04 +0100

    Dzień 13 2.0

--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -34,11 +34,11 @@ namespace ChallengeApp
         {
             if (float.TryParse(grade, out float result))
             {
-                this.AddGrade(result);
+                AddGrade(result);
             }
             else if (char.TryParse(grade, out char result2))
             {
-                this.AddGrade(result2);
+                AddGrade(result2);
             }    
             else
             {
@@ -87,26 +87,26 @@ namespace ChallengeApp
             {
                 case 'A':
                 case 'a':
-                    this.grades.Add(100);
+                    AddGrade(100);
                     break;
                 case 'B':
                 case 'b':
-                    this.grades.Add(80);
+                    AddGrade(80);
                     break;
                 case 'C':
                 case 'c':
-                    this.grades.Add(60);
+                    AddGrade(60);
                     break;
                 case 'D':
                 case 'd':
-                    this.grades.Add(40);
+                    AddGrade(40);
                     break;
                 case 'E':
                 case 'e':
-                    this.grades.Add(20);
+                    AddGrade(20);
                     break;
                 default:
-                    throw new Exception($"{grade} to nieprawidłowa litera litera");
+                    throw new Exception($"{grade} to nieprawidłowa litera");
             }
         }
 
